b7cf7f5151c5482cfe3dc657f46968a965eb3406
Cole Perullo <cperullo@usf.edu>
2026-06-14 16:19:29 -0400

    Finished step 3, made manager class in .js

--- cc_6.js
+++ cc_6.js
@@ -9,3 +9,15 @@ class Employee {
         return `${this.name} works in the ${this.department} department.`;
     }
 }
+
+// Step 3: Create Manager class that extends Employee
+class Manager extends Employee {
+    constructor(name, department, teamSize) {
+        super(name, department);
+        this.teamSize = teamSize;
+    }
+
+    describe() {
+        return `${this.name} is a manager in the ${this.department} department and manages a team of ${this.teamSize} employees.`;
+    }
+}
